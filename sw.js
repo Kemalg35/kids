8e607c1473d37c2073bd7bc8d95c45c4a75c3025
Kemalg35 <kml.cns.35@gmail.com>
2025-11-03 20:56:29 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const C="minik-v1";
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(C).then(c=>c.addAll([
-    "./","./index.html","./style.css","./app.js","./manifest.json"
-  ])));
+const C = "minikatolye-v1";
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(C).then(c=>c.addAll(["./","./index.html","./style.css","./app.js"]))
+  );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
